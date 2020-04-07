286cf6b430eaeb7542d94f7e0d64c90c2589bac2
MD HASIBUR RAHMAN LEMON <58658662+hasiburrahman123@users.noreply.github.com>
2020-04-07 16:09:44 +0800

    Add files via upload

---
+++ URI_1001_solution.cpp
@@ -0,0 +1,9 @@
+    #include <iostream>
+    using namespace std;
+    int main() {
+       int A, B, X;
+       cin >> A >> B;                //take input for A and B
+       X = A + B;                   //Basic summation X = A + B
+       cout << "X = " << X << endl; //Print X in given format and endl is endline or it works linke "\n" in c and java
+       return 0;
+    }
