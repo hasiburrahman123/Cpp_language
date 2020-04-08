1dca468c8f950ab01d997f4968497f1f5f5ca88c
MD HASIBUR RAHMAN LEMON <58658662+hasiburrahman123@users.noreply.github.com>
2020-04-08 18:10:28 +0800

    Add files via upload

---
+++ URI_1008_solution.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+using namespace std;
+int main(){
+int a;
+int b;
+float c;
+
+scanf("%d", &a);
+scanf("%d",&b);
+scanf("%f",&c);
+
+printf("NUMBER = %d\n",a);
+printf("SALARY =  %.2f\n", b*c);
+
+return 0;
+
+}
